3b9d356adb68e4857de825144ec305ae69e6af25
JosephKevinMachado <joseph.machado.professional@gmail.com>
2024-07-29 17:25:27 -0400

    2024-07-29-17-25-27 - insert-only-new-rows

--- ecommerce/ecommerce/dim_customer_etl.py
+++ ecommerce/ecommerce/dim_customer_etl.py
@@ -12,7 +12,7 @@ def write_non_validated_base_state(db_cursor):
             CAST(state_id AS INTEGER) AS state_id,
             CAST(state_code AS TEXT) AS state_code,
             CAST(state_name AS TEXT) AS state_name
-        FROM raw_state;
+        FROM raw_state
         """
     )
 
@@ -25,7 +25,8 @@ def publish_base_state(db_cursor):
             state_code,
             state_name,
             datetime('now') AS etl_inserted
-        FROM non_validated_base_state;
+        FROM non_validated_base_state
+        WHERE state_id not in (select state_id from base_state);
         """
     )   
 
@@ -56,7 +57,8 @@ def publish_base_customer(db_cursor):
             datetime_created,
             datetime_updated,
             datetime('now')
-        FROM non_validated_base_customer;
+        FROM non_validated_base_customer
+        WHERE customer_id not in (select customer_id from base_customer);
         """
     )
 
@@ -64,7 +66,7 @@ def write_non_validated_dim_customer(db_cursor):
     db_cursor.execute(
         """
         INSERT INTO non_validated_dim_customer (customer_id, zipcode, city, state_code, state_name, datetime_created, datetime_updated)
-        SELECT
+        SELECT DISTINCT
             c.customer_id,
             c.zipcode,
             c.city,
@@ -82,16 +84,16 @@ def publish_dim_customer(db_cursor):
         """
         INSERT INTO dim_customer(customer_id, zipcode, city, state_code, state_name, datetime_created, datetime_updated, etl_inserted)
         SELECT
-            c.customer_id,
-            c.zipcode,
-            c.city,
-            c.state_code,
-            s.state_name,
-            c.datetime_created,
-            c.datetime_updated,
+            customer_id,
+            zipcode,
+            city,
+            state_code,
+            state_name,
+            datetime_created,
+            datetime_updated,
             datetime('now')
-        FROM base_customer AS c
-        LEFT JOIN base_state AS s ON c.state_code = s.state_code;
+        FROM non_validated_dim_customer
+        where customer_id not in (select customer_id from dim_customer);
         """
     )
     pass
@@ -137,11 +139,6 @@ def run():
     conn = sqlite3.connect('ecommerce.db')
     cursor = conn.cursor()
 
-    cursor.execute("DELETE FROM non_validated_dim_customer;")
-    cursor.execute("DELETE FROM non_validated_base_customer;")
-    cursor.execute("DELETE FROM non_validated_base_state")
-    conn.commit()
-
     # NOTE: WRITE -> AUDIT -> PUBLISH pattern
     write_non_validated_base_customer(cursor)
 
@@ -167,12 +164,17 @@ def run():
 
     dim_customer_validation_result = audit('non_validated_dim_customer')
     dim_customer_count_anomaly = audit('dim_customer_dt_created_count')
-    if check_audit_failures(dim_customer_validation_result):
+    if check_audit_failures(dim_customer_validation_result) and check_audit_failures(dim_customer_count_anomaly):
         publish_dim_customer(cursor)
     else:
         print("======== dim_customer DQ check failed ==========")
         print(dim_customer_validation_result)
         sys.exit(1)
+
+    cursor.execute("DELETE FROM non_validated_dim_customer;")
+    cursor.execute("DELETE FROM non_validated_base_customer;")
+    cursor.execute("DELETE FROM non_validated_base_state")
+    conn.commit()
 
     # Commit the changes and close the connection
     conn.commit()
